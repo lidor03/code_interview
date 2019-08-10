343c58932692c0c5c1e470566542256fa5a586db
lidor03 <syndromejs@gmail.com>
2019-08-10 17:53:45 +0300

    add Q 2

--- interview_codes.py
+++ interview_codes.py
@@ -23,9 +23,28 @@ def is_unique_clean(str):
         first = False
     return True
 
+# Q 1.2
+
+def check_permutation(str1, str2):
+    #check if str2 is a permutation of str1
+    my_dict = {}
+    for i in range(256):
+        my_dict[chr(i)] = 0
+    for a in str1:
+        my_dict[a] += 1
+    for b in str2:
+        my_dict[b] -= 1
+    for i in range(256):
+        if my_dict[chr(i)] != 0:
+            return False
+    return True
+
 def main():
     print(is_unique("afdgfj"))
     print(is_unique_clean("afdgfj"))
+    print(check_permutation("123456", "654321"))
+    print(check_permutation("123456", "112233445566"))
+
 
 
 
