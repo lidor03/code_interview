8543717b9fba9b3849b20535ed2cce1463b4ba30
lidor03 <syndromejs@gmail.com>
2019-08-11 15:23:00 +0300

    Q 5

--- CodeInterview.java
+++ CodeInterview.java
@@ -35,10 +35,31 @@ public class CodeInterview {
 		return numOfOdd <= 1;
 	}
 	
+	//Q 5
+	
+	public static boolean oneAway(String str1, String str2){
+		String mystr1=str1, mystr2 = str2;
+		for (int i=0; i<str1.length() && i<str2.length(); i++){
+			int j1 = i, j2 = i+1;
+			if(str1.charAt(i) != str2.charAt(i)){
+				if(str1.length()>str2.length()){
+					mystr1 = str2;
+					mystr2 =str1;
+				}
+				else if (str1.length() == str2.length()){
+					j1++;
+				}
+				return mystr1.substring(0,i).equals(mystr2.substring(0,i)) && mystr1.substring(j1).equals(mystr2.substring(j2));
+			}
+		}
+		return str1.equals(str2);
+	}
+	
 	public static void main(String[] args){
 		char[] arr = "Mr John Smith    ".toCharArray();
 		uRLify(arr , 13);
 		System.out.println(new String(arr));
 		System.out.println(palindromePermutation("Tact Coal"));
+		System.out.println(oneAway("pale", "bale"));
 	}
 }
